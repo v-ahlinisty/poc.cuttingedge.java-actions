515d45f0f4cdb3948d5bac0ea757d2b57be78368
Anton Hlinisty <v-ahlinisty@ebsco.com>
2019-11-21 12:48:41 +0300

    Add docker build and publish steps

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -1,9 +1,30 @@
 #!/bin/sh -l
 
+echo ""
 echo "Run unit tests"
+echo ""
 echo "--------------"
+echo ""
 
 ./gradlew build
 
-echo "print custom parameter"
-echo "app name: $3"
+echo ""
+echo "Login to dockerhub"
+echo "$2 | docker login --username $1 --password-stdin"
+echo ""
+echo "--------------"
+echo ""
+
+echo ""
+echo "Build docker image"
+echo "docker build -t antonhlinistyepam/$3 ."
+echo ""
+echo "--------------"
+echo ""
+
+echo ""
+echo "Push an image to dockerhub"
+echo "docker push antonhlinistyepam/$3"
+echo ""
+echo "--------------"
+echo ""
